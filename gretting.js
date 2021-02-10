cf66b7b5f23172e9d0213778b84c79c2528c4bb2
dowk12 <stonerl12@naver.com>
2021-02-11 02:20:05 +0900

    #11. result

--- gretting.js
+++ gretting.js
@@ -22,8 +22,8 @@ function askForName() {
 }
 
 function paintGreeting(text) {
-    //form.classList.remove(SHOWING_CN);
-    //greeting.classList.add(SHOWING_CN);
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
 

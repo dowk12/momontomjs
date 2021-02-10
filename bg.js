30ccfd59b2060525a27a02d94f657fe1c7c6911b
dowk12 <stonerl12@naver.com>
2021-02-10 20:54:57 +0900

    #8. Image Background

---
+++ bg.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 9;
+
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`;
+    image.classList.add("bgImage");
+    body.appendChild(image);
+    console.log(image);
+}
+
+function genRandom() {
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init() {
+    const randomNumeber = genRandom();
+    paintImage(randomNumeber)
+}
+
+init();

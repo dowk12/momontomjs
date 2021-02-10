51ae19af804fce16206f3e51d675ef337a4afd7d
dowk12 <stonerl12@naver.com>
2021-02-10 21:45:58 +0900

    #10. Getting the Weather part2
    with API

--- weather.js
+++ weather.js
@@ -1,5 +1,19 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY ="8eeb1976e137911eddbdf4a7d4ed18be";
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    )
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json) {
+            const temperture = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperture} @ ${place}`;
+        });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +27,7 @@ function hadleGeoSuccess(position) {
         longitude: longitude  //위와 동일함
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude)
 }
 
 function handleGeoError() {
@@ -24,11 +39,12 @@ function askForCoords() {
 }
 
 function loadCoords() {
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords === null) {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) {
         askForCoords();
     } else {
-        //getWeather
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
